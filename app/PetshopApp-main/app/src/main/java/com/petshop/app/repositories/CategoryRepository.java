9baf9073688a8a6b0ae833b59d3c6f9248a6143c
NicolasAlfonsin <128539415+NicolasAlfonsin@users.noreply.github.com>
2026-09-11 02:40:04 -0300

    Delete app/PetshopApp-main directory

--- app/PetshopApp-main/app/src/main/java/com/petshop/app/repositories/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.petshop.app.repositories;
-
-import com.petshop.app.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, String> {
-}
